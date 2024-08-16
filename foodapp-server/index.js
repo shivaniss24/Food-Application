4044c4ce98a74f65e0c1ac98dc5fd382ab36044e
Shivani Soni <eng.shivanisoni2405@gmail.com>
2024-08-16 16:26:17 +0530

    updated db

--- foodapp-server/index.js
+++ foodapp-server/index.js
@@ -1,32 +1,24 @@
-const express =require ('express');
-var cors = require('cors')
+const express=require('express');
 const app=express();
-const PORT=process.env.PORT || 3000;
-require('dotenv').config()
+const cors=require("cors");
+const PORT = process.env.PORT || 3000;
+require('dotenv').config();
 console.log(process.env.DB_USER);
+console.log(process.env.DB_PASSWORD);
 
 
-// username: engshivanisoni2405
-// password: dTYiQfDfjYOKPono
 
-// middle ware for interaction cors
+// username- engshivanisoni2405
+// password-tjZcJK3VUVlPHmLR
+
+
+// middleware for interaction
 app.use(cors());
 app.use(express.json());
 
-app.get("/",(req,res)=>{
-    res.send("hello world!");
-})
-
-// log all registered routes
-// console.log("Registered Routes:",app._router.stack);
-
-app.listen(PORT,()=>{
-    console.log(`App started on port: ${PORT}`)
-})
-
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.lex8v.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = "mongodb+srv://shivanisoni:Shivanipassword1@shivani.bvehk.mongodb.net/?retryWrites=true&w=majority&appName=shivani";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -34,37 +26,62 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  },
-
+  }
 });
+
+ // database and collections
+ const menuCollections = client.db("Database-Food").collection("menu");
+ const cartCollections = client.db("Database-Food").collection("cartItems");
+
+
+  // all menu items 
+  app.use("/menu",async(req,res)=>{
+   console.log("reached to menu route");
+   const result= await menuCollections.find().toArray();
+   res.send(result);
+   // const database = client.db("sample_mflix");
+   // const movies = database.collection("movies");
+ });
+
+
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    console.log("attempting to connect");
     await client.connect();
-    console.log("connected");
-
-    //  database and collections
-      const  menuCollections = client.db("foodapp-client").collection("menus");
-
-      const cartCollections = client.db("foodapp-client").collection("cartItems");
-
-    //   all menu items
-    app.get('/menu',async(req,res)=>{
-        const result = await menuCollections.find().toArray()
-        res.send(result);
-     
-    })
-
-
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-
     // Ensures that the client will close when you finish/error
-    await client.close();
+    await client.connect();
   }
 }
 run().catch(console.dir);
+
+
+app.get("/",(req,res)=>{
+  res.send("hello world");
+})
+
+
+
+// log all registered routes
+console.log("Registered Routes:",app._router.stack);
+
+app.listen(PORT,()=>{
+  console.log(`App started on port: ${PORT}`)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
